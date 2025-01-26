c10f8d2184f030182b40f81f00d49ba5ade60df7
Gopinath M <gopicool.m@gmail.com>
2025-01-26 19:55:55 +0530

    environment files upload

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  perplexityApiKey: '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',
+  openAiApiKey: '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'
+};
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  perplexityApiKey: 'your-perplexity-api-key',
+  openAiApiKey: 'your-openai-api-key'
+};
